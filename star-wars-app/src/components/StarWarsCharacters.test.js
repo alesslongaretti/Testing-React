980cecd668adeee1137323dce75f5deea2020944
alesslongaretti <alesslongaretti@gmail.com>
2020-02-12 23:28:42 -0500

    tests for the buttons are working

---
+++ star-wars-app/src/components/StarWarsCharacters.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { render, fireEvent, act } from "@testing-library/react";
+import StarWarsCharacters from "./StarWarsCharacters";
+
+it('renders "Previous" button', () => {
+  const { getByText, findByText } = render(<StarWarsCharacters />);
+
+  act(() => {
+    fireEvent.click(getByText("Previous"));
+  });
+  findByText(/previous/i);
+});
+
+it('renders "Next" button', () => {
+  const { getByText, findByText } = render(<StarWarsCharacters />);
+
+  act(() => {
+    fireEvent.click(getByText("Next"));
+  });
+  findByText(/next/i);
+});
